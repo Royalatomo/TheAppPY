3665428e6d56925a4b504ab4ad61008971508285
BINGOO <54973413+Royalatomo@users.noreply.github.com>
2020-02-19 03:42:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,437 @@
+try:
+
+    import name
+
+    print(name.con())
+
+# Always Want To Learn More And More
+
+    try:
+
+        where_key = input('Confirm Your Path: ')
+
+        import os
+
+        os.chdir(where_key)
+
+        filename = 'Password.txt'
+
+        with open(filename, 'r') as files:
+
+            data = files.read().replace('\n', '')
+
+            word = input('confirm Your Email: ')
+            emoji = {
+
+                'a': '@1*(&SDjfhk',
+                'b': '56*(&JDH(',
+                'c': 'rgSDF^&H4',
+                'd': 'g&*^JHs9h',
+                'e': '#2^&UHy78sd',
+                'f': 'y2&*%SD^*fs',
+                'g': '8*&SD^fuh*',
+                'h': '&7*(&ETIJsd',
+                'i': '%t*&DJKfh',
+                'j': '6@3489jhf',
+                'k': '5sadfh6',
+                'l': '2368sd7f',
+                '.': 'l345sd',
+                'm': 'h564dfda',
+                'n': 'o&*(&DSk)',
+                'o': '*&SD*F^j9',
+                'p': 'PSDF32445^',
+                'q': '90s75df34',
+                'r': 'sd%#$@r5sf',
+                's': 'T&Sdf6usd1j',
+                't': 'PSD*Ifu908P',
+                'u': 'Uv*()&Df',
+                'w': '<Ks45sd4ff',
+                'x': 'DSDf5445q',
+                'y': '{K#$%$L',
+                'z': '<LS&*f64sd',
+                '@': 'OS(D*&f4I',
+                'A': 'PLWr52d',
+                'B': ')*r2sd',
+                'C': 'KHWER524sd',
+                'D': '&&$W5232sd',
+                'E': 'D#$r1^',
+                'F': '&*$3rdsdf$',
+                'G': '*5ASasd',
+                'H': '@!$#Rsdsdaf!',
+                'I': '{DFWer455}sdrt',
+                'J': '{*7SFd}ad2f1',
+                'K': '[9$]12sd52wf',
+                'L': '+|@#$%sdfa+',
+                'M': '~`#@$1',
+                'N': '^$4WER2df',
+                'O': '0=8(*Sdfjsdf7',
+                'P': 'DF(S*D7ffsd455',
+                'Q': '%55#W#$sd32f1',
+                'R': '|?/W$er63D',
+                'S': 'KJD^RF4d&',
+                'T': '<RAHUSE4we4a4r2weL>',
+                'U': 'B|@WErd',
+                'V': '@B|?Erf2s>sd*',
+                'W': ' ?DF>we2r2sdaf?',
+                'X': '+_0#WEr41sdf%',
+                'Y': '9&_($%$50)WE2rsd2',
+                'Z': '{RAHUL2sf2sd}{GRwe2rfw2efANTED}er1w12fs',
+                '1': '56@$rf2s1f',
+                '2': '76$W%5wsd',
+                '3': '3469s4qwe15',
+                '4': '7ASF52sf68345wf8',
+                '6': 'SD45SEr15sd',
+                '7': 'sdf43Ser41345@#45',
+                '8': '5th5#$%$121wer4',
+                '9': '10thd$%5412ffs',
+                '0': '54sdW$54sd2ff'
+            }
+            output = ''
+            for words in word:
+                output += emoji.get(words)
+            password = output
+
+            if not password in data:
+
+                print('Wrong Email: ')
+
+                exit()
+
+            else:
+
+                print('Give The path where you stored TheApp Folder like in Desktop or in other directories')
+
+                where = input('Where you stored this programme(path): ')
+
+                os.chdir(where)
+
+                print('Start This Programme again!! ')
+
+                os.remove(where + '/TheApp/name.py')
+
+    except FileNotFoundError:
+
+        print('Inlaid Path')
+
+        exit()
+
+except ModuleNotFoundError:
+
+    where = input(' Key Path:  ')
+
+    try:
+
+# Be The First To Use Anything Otherwise Time Make The Thing Old
+
+        try:
+
+            import os
+
+            os.chdir(where)
+
+            filename = 'Password.txt'
+
+            with open(filename, 'r') as files:
+
+                data = files.read().replace('\n', '')
+
+                rahul = data
+
+                email = input('Email: ')
+
+                word = email
+                emoji = {
+
+                    'a': '@1*(&SDjfhk',
+                    'b': '56*(&JDH(',
+                    'c': 'rgSDF^&H4',
+                    'd': 'g&*^JHs9h',
+                    'e': '#2^&UHy78sd',
+                    'f': 'y2&*%SD^*fs',
+                    'g': '8*&SD^fuh*',
+                    'h': '&7*(&ETIJsd',
+                    'i': '%t*&DJKfh',
+                    'j': '6@3489jhf',
+                    'k': '5sadfh6',
+                    'l': '2368sd7f',
+                    '.': 'l345sd',
+                    'm': 'h564dfda',
+                    'n': 'o&*(&DSk)',
+                    'o': '*&SD*F^j9',
+                    'p': 'PSDF32445^',
+                    'q': '90s75df34',
+                    'r': 'sd%#$@r5sf',
+                    's': 'T&Sdf6usd1j',
+                    't': 'PSD*Ifu908P',
+                    'u': 'Uv*()&Df',
+                    'w': '<Ks45sd4ff',
+                    'x': 'DSDf5445q',
+                    'y': '{K#$%$L',
+                    'z': '<LS&*f64sd',
+                    '@': 'OS(D*&f4I',
+                    'A': 'PLWr52d',
+                    'B': ')*r2sd',
+                    'C': 'KHWER524sd',
+                    'D': '&&$W5232sd',
+                    'E': 'D#$r1^',
+                    'F': '&*$3rdsdf$',
+                    'G': '*5ASasd',
+                    'H': '@!$#Rsdsdaf!',
+                    'I': '{DFWer455}sdrt',
+                    'J': '{*7SFd}ad2f1',
+                    'K': '[9$]12sd52wf',
+                    'L': '+|@#$%sdfa+',
+                    'M': '~`#@$1',
+                    'N': '^$4WER2df',
+                    'O': '0=8(*Sdfjsdf7',
+                    'P': 'DF(S*D7ffsd455',
+                    'Q': '%55#W#$sd32f1',
+                    'R': '|?/W$er63D',
+                    'S': 'KJD^RF4d&',
+                    'T': '<RAHUSE4we4a4r2weL>',
+                    'U': 'B|@WErd',
+                    'V': '@B|?Erf2s>sd*',
+                    'W': ' ?DF>we2r2sdaf?',
+                    'X': '+_0#WEr41sdf%',
+                    'Y': '9&_($%$50)WE2rsd2',
+                    'Z': '{RAHUL2sf2sd}{GRwe2rfw2efANTED}er1w12fs',
+                    '1': '56@$rf2s1f',
+                    '2': '76$W%5wsd',
+                    '3': '3469s4qwe15',
+                    '4': '7ASF52sf68345wf8',
+                    '6': 'SD45SEr15sd',
+                    '7': 'sdf43Ser41345@#45',
+                    '8': '5th5#$%$121wer4',
+                    '9': '10thd$%5412ffs',
+                    '0': '54sdW$54sd2ff'
+                }
+                output = ''
+                for words in word:
+                    output += emoji.get(words)
+                email = output
+
+                if email != rahul:
+
+                    print('Wrong Email: ')
+
+                    print("RETRY MR. ")
+
+                    exit()
+
+                else:
+
+                    print('LOGIN SUCCESSFULLY ::')
+
+        except FileNotFoundError:
+
+            print('Wrong Path: ')
+
+            exit()
+        try:
+
+            print('''
+    
+             _   _       _           
+            | \ | | ___ | |_ ___   _ 
+            |  \| |/ _ \| __/ _ \ (_)       YOU CAN SAVE ANY DATA IN A FILE FOR ONLY ONE TIME NEXT TIME IF YOU TRY TO ADD 
+            | |\  | (_) | ||  __/  _        DATA IN THE SAME USERNAME THEN THE PREVIOUS NEW DATA WILL OVERWRITE THE OLD DATA 
+            |_| \_|\___/ \__\___| (_)       BE AWARE:::
+                                     
+            
+            
+            ''')
+
+            print('#########  Please Read The Above Note ##############')
+
+            import os
+
+            os.chdir(where)
+
+            os.mkdir('passwords/')
+
+            while True:
+
+# Copyrighted
+
+                print('USE [H] TO GET HELP ')
+
+                action = input('>> ').upper()
+                if action == 'E':
+
+                    import os
+
+                    import Test
+
+                    os.chdir(where + '/passwords/')
+
+                    Test.test()
+
+                elif action == 'H':
+
+                    print("""
+    
+                                                use [E]  :  % USE 'E' KEYWORD TO ENTER THE DATA IN THE USERNAME % 
+    
+                                                use [S]  :  % USE 'S' KEYWORD TO SEE THE DATA SAVED IN THE USERNAME  %
+    
+                                                use [H]  :  % USE 'H' KEYWORD TO SEE THE HELP LIKE THIS %
+    
+                                                use [Q]  :  % USE 'Q' KEYWORD TO QUITE FROM THE APPLICATION %
+    
+                                         """)
+
+                        # Never Think You Have Learnt everything
+
+                elif action == 'Q':
+
+                    print('Thanks for using our application ')
+
+                    print(''' 
+    
+                                            ┌───────────────────────────────────────────────────────┐
+                                            │ ▙▗▌▞▀▖▛▀▖▛▀▘   ▛▀▖▌ ▌   ▀▛▘▌ ▌▛▀▘   ▛▀▖▜▘▙ ▌▞▀▖▞▀▖▐▐  │
+                                            │ ▌▘▌▙▄▌▌ ▌▙▄    ▙▄▘▝▞     ▌ ▙▄▌▙▄    ▙▄▘▐ ▌▌▌▌▄▖▌ ▌▐▐  │
+                                            │ ▌ ▌▌ ▌▌ ▌▌     ▌ ▌ ▌     ▌ ▌ ▌▌     ▌ ▌▐ ▌▝▌▌ ▌▌ ▌▝▝  │
+                                            │ ▘ ▘▘ ▘▀▀ ▀▀▘   ▀▀  ▘     ▘ ▘ ▘▀▀▘   ▀▀ ▀▘▘ ▘▝▀ ▝▀ ▝▝  │
+                                            └───────────────────────────────────────────────────────┘
+    
+    
+                                        ''')
+
+                    exit()
+
+                elif action == 'S':
+                    import os
+                    os.chdir(where + '/passwords/')
+                    import ifs
+                    print(ifs.ifs())
+
+
+
+
+# Always Learn SOmething
+
+
+        except FileNotFoundError:
+
+            print('''SOrry The path you set is not correct or we are not able to reach there
+    
+                     or try to use the correct spelling of the Directories .. ''')
+
+    except FileExistsError:
+
+        try:
+            print('''We Find That there is already a file: If you want to use the same file then Press  "N"
+                            OR If you want new Press "Y":''')
+
+            tell = input("(DEFAULT - Y) > ").lower()
+
+            if tell == '':
+
+                print('Ok TRY a new path then: ')
+
+                exit()
+
+            if tell == 'y':
+
+                print('Ok TRY a new path then: ')
+
+                exit()
+
+            if tell == 'n':
+
+                pass
+
+            else:
+
+                print("Invalid Option ")
+
+                exit()
+
+
+
+            while True:
+
+                print('USE [H] TO SEE THE OPTIONS ')
+
+                action = input('>> ').upper()
+
+                if action == 'E':
+
+                    import os
+
+                    import Test
+
+                    os.chdir(where + '/passwords/')
+
+                    Test.test()
+
+                if action == 'H':
+
+                    print("""
+        
+                                use [E]  :  % USE 'E' KEYWORD TO ENTER THE DATA IN THE USERNAME % 
+        
+                                use [S]  :  % USE 'S' KEYWORD TO SEE THE DATA SAVED IN THE USERNAME  %
+        
+                                use [H]  :  % USE 'H' KEYWORD TO SEE THE HELP LIKE THIS %
+        
+                                use [Q]  :  % USE 'Q' KEYWORD TO QUITE FROM THE APPLICATION %
+                       
+        
+                         """)
+                if action == 'h':
+
+# Programming Is The Art Not All Get It
+
+                    print("""
+        
+                                use [E]  :  % USE 'E' KEYWORD TO ENTER THE DATA IN THE USERNAME % 
+        
+                                use [S]  :  % USE 'S' KEYWORD TO SEE THE DATA SAVED IN THE USERNAME  %
+        
+                                use [H]  :  % USE 'H' KEYWORD TO SEE THE HELP LIKE THIS %
+        
+                                use [Q]  :  % USE 'Q' KEYWORD TO QUITE FROM THE APPLICATION %
+        
+        
+                        """)
+
+                if action == 'Q':
+
+                    print('Thanks for using our application ')
+
+                    print(''' 
+                            Always Learning in Cyber world !!
+                            ┌───────────────────────────────────────────────────────┐
+                            │ ▙▗▌▞▀▖▛▀▖▛▀▘   ▛▀▖▌ ▌   ▀▛▘▌ ▌▛▀▘   ▛▀▖▜▘▙ ▌▞▀▖▞▀▖▐▐  │
+                            │ ▌▘▌▙▄▌▌ ▌▙▄    ▙▄▘▝▞     ▌ ▙▄▌▙▄    ▙▄▘▐ ▌▌▌▌▄▖▌ ▌▐▐  │
+                            │ ▌ ▌▌ ▌▌ ▌▌     ▌ ▌ ▌     ▌ ▌ ▌▌     ▌ ▌▐ ▌▝▌▌ ▌▌ ▌▝▝  │
+                            │ ▘ ▘▘ ▘▀▀ ▀▀▘   ▀▀  ▘     ▘ ▘ ▘▀▀▘   ▀▀ ▀▘▘ ▘▝▀ ▝▀ ▝▝  │
+                            └───────────────────────────────────────────────────────┘
+                                                                Always Learning in Cyber WOrld !!
+                            ''')
+
+                    exit()
+
+                if action == 'S':
+                    import os
+
+                    os.chdir(where + '/passwords/')
+
+                    import ifs
+
+                    print(ifs.ifs())
+
+
+
+
+        except FileNotFoundError:
+
+            print('''SOrry The path you set is not correct or we are not able to reach there
+    
+            or try to use the correct spelling of the Directories .. ''')
+
+            exit()
+
+# Thank You ::
